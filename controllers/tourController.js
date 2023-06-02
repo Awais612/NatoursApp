bf2ca4348c7f7638fbf1bc87a5257f6fe7fa8489
Muhammad Awais Yousaf Butt <buttawa0000@gmail.com>
2023-06-03 01:57:31 +0500

    CRUD
    CRUD performed using the mongoos driver. Changes the RESTAPI with the mongoos method to perform CRUD

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,93 +1,99 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-// Get Request Handler
-// FILE READING
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour Id is: ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid ID',
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
     });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next)=>{
-  if(!req.body.name || !req.body.price){
-    return res.status(400).json({
+  } catch (err) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Missing name or price for the tour'
+      message: err,
     });
   }
-  next();
-}
-
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    requestedAt: req.requestTime,
-    data: {
-      tours,
-    },
-  });
 };
 
 // Handling the URL requests
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours: tour,
-    },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id); //shorthand to write as the follow sccript
+    // Tour.findOne({ _id: req.param.id }) same working in the mongoShell as above
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // Post Request Handler
-exports.postTour = (req, res) => {
-  // console.log(req.body);
-  // Saving the data to the file in the json format
-  const newID = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newID }, req.body);
-  tours.push(newTour);
-  // Writing to the file
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      console.log('Error Occurred while writing data to the file!', err);
-      res.status(201).json({
-        status: 'Success',
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'Success',
+      data: {
         tour: newTour,
-      });
-    }
-  );
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
 // Patch request handler
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<.......Updated tour here......>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // Delete Request Handler
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ mongoose.connect(DB, {
 }).then(() => {
   console.log('DB connection successful');
 });
+
 const port =process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}`);
