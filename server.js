decfd548f4defa1af098ab14e5f669d7c15f3191
Muhammad Awais Yousaf Butt <buttawa0000@gmail.com>
2023-06-10 19:19:00 +0500

    Handled uncaught exceptions

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console
+      .log('DB connection successful')
+      .catch((err) => console.log('Error'));
+  });
 
-mongoose.connect(DB, {
-  useNewUrlParser:true,
-}).then(() => {
-  console.log('DB connection successful');
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port: ${port}`);
 });
 
-const port =process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port: ${port}`);
+// Handling the unhandled rejections
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 SHUTTING DOWN...');
+  console.log(err.name, err.message);
+  // process.exit(1); Not a preferred way to shutdown a application. As it suddenly shutdown all the processes.
+
+  server.close(() => {
+    // It will first close the server and then shutdown the application
+    process.exit();
+  });
+});
+
+// Catching uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED Exception! 💥 SHUTTING DOWN...');
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit();
+  });
 });
