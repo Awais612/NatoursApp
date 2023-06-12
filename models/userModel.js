04311baac1932845109807b9a0e0b90704e0732e
Muhammad Awais Yousaf Butt <buttawa0000@gmail.com>
2023-06-13 01:46:57 +0500

    Password Encryption
    Password encryption has been implementd!

--- models/userModel.js
+++ models/userModel.js
@@ -1,40 +1,53 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 /*schema for the user
- *name, email, photo, passwords, passwordConfirm 
-*/
+ *name, email, photo, passwords, passwordConfirm
+ */
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type: String,
-        required: [true, 'Please tell us your name']
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter your valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'You must have to setup the password for the account.'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        // This only works only for the CREATE & SAVE!!!!
+        return el === this.password; // abc === abc it'll give the boolean value either true or false.
+      },
+      message: 'Passwords are not the same!',
     },
-    email: {
-        type: String,
-        required: [true, 'Please enter your email'],
-        unique: true,
-        lowercase: true,
-        validate: [validator.isEmail, 'Please enter your valid email'],
-    },
-    photo: {
-        type: String,
-    },
-    password: {
-        type: String,
-        required: [true, 'You must have to setup the password for the account.'],
-        minlength: 8
-    },
-    passwordConfirm: {
-        type: String,
-        required: [true, 'Please confirm your password'],
-        validate: function(el){
-            // This only works only for the CREATE & SAVE!!!!
-            return el === this.password; // abc === abc it'll give the boolean value either true or false.
-
-        }
-    }
+  },
 });
 
-const User = mongoose.model('User',userSchema);
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+
+    // Password encryption with the hash cost of 12 
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
 module.exports = User;
